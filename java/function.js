16573c28b84149e7af79c06502645db8f24c09a9
ALWINgm <150226233+ALWINgm@users.noreply.github.com>
2024-05-21 02:18:57 -0700

    Add files via upload

---
+++ java/function.js
@@ -0,0 +1,10 @@
+function add(a,b){
+    let c=a+b;
+    console.log(c);
+}
+add(1,2);
+function fullname(fname, iname)
+{
+    console.log(`${fname} ${iname}`)
+}
+fullname("alwin", "mathew")
---
+++ java/script.js
@@ -0,0 +1,30 @@
+//array
+//let fruits=["apple", "mango","orange",23]
+//console.log(fruits);
+//fruits.push("grapes");
+//console.log(fruits);
+//console.log(fruits.length);
+//fruits[2]="pineapple";
+//console.log(fruits);
+//fruits.unshift("pear");
+//console.log(fruits)
+//fruits.pop();
+//console.log(fruits)
+//fruits.shift();
+//console.log(fruits);
+let bsc =[ 
+    {
+        fname: "anto",
+        age: 10,
+    },
+    {
+      fname:"christo",
+      age:15,
+    },
+    {
+        fname:"elias",
+        age:19,
+    },
+];
+console.log(bsc[0].age);
+
